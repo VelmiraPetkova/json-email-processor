1128ea66d1c4bab10159468c2dc868333b85b9f2
Velmira Georgieva <104137851+VelmiraPetkova@users.noreply.github.com>
2024-11-22 13:22:58 +0200

    Initial commit

---
+++ test/aws-prep.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsPrep from '../lib/aws-prep-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-prep-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsPrep.AwsPrepStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
